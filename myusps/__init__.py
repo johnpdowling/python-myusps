4578bd9b703269e3ebdbec41bcfacce854d43352
John Dowling <john.patrick.dowling@gmail.com>
2018-09-21 15:47:48 -0700

    back to normal?

--- myusps/__init__.py
+++ myusps/__init__.py
@@ -146,7 +146,7 @@ def _get_driver(driver_type):
     if driver_type == 'phantomjs':
         return webdriver.PhantomJS(service_log_path=os.path.devnull)
     if driver_type == 'firefox':
-        return webdriver.Firefox(executable_path=r'/share/bin/geckodriver', firefox_options=FIREFOXOPTIONS)
+        return webdriver.Firefox(firefox_options=FIREFOXOPTIONS)
     elif driver_type == 'chrome':
         chrome_options = webdriver.ChromeOptions()
         for arg in CHROME_WEBDRIVER_ARGS:
