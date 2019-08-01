31a42bcfb9928dd6167b8b16fc3146b478d5fea0
John Dowling <john.patrick.dowling@gmail.com>
2019-08-01 12:47:48 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='myusps',
-    version='1.3.2',
+    version='1.3.3',
     description='Python 3 API for USPS Informed Delivery, a way to track packages and mailpieces.',
     url='https://github.com/happyleavesaoc/python-myusps/',
     license='MIT',
     author='happyleaves',
     author_email='happyleaves.tfr@gmail.com',
     packages=find_packages(),
-    install_requires=['beautifulsoup4==4.6.0', 'python-dateutil==2.6.0', 'requests>=2.20.0', 'requests-cache==0.4.13', 'selenium==3.11.0'],
+    install_requires=['beautifulsoup4>=4.6.0', 'python-dateutil>=2.6.0', 'requests>=2.20.0', 'requests-cache>=0.4.13', 'selenium>=3.11.0'],
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
